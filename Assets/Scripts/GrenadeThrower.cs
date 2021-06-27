d8bf5e28db6482643dc3c8d48c70f4f300c87efc
NirBD <62333490+NirbDor@users.noreply.github.com>
2021-06-27 12:06:29 +0300

    Update: Entire Map

--- Assets/Scripts/GrenadeThrower.cs
+++ Assets/Scripts/GrenadeThrower.cs
@@ -10,7 +10,8 @@ public class GrenadeThrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             ThrowGrenade();
         }
     }
