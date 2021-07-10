3b7e2ef1d0d4032c9b78dda39f76639756e5355e
Arielv1 <arielv1994@gmail.com>
2021-07-10 12:20:46 +0300

    Fixed weapon particle display bug & added laser particle systems
    Fixed weapon particle display bug & added laser particle systems

--- Assets/Scripts/Player/PlayerLaserShooting.cs
+++ Assets/Scripts/Player/PlayerLaserShooting.cs
@@ -12,15 +12,18 @@ public class PlayerLaserShooting : MonoBehaviour
     public float hitRadius = 2f;
     public float hitForce = 2500f;
     public GameObject laserGun;
+    public GameObject muzzlePoint;
     private Camera fpsCamera;
     public ParticleSystem muzzleFlash;
+    private LineRenderer lr;
 
     public static int LASER_MAX_AMMO = 3;
-    private int ammoLeft;
+    [SerializeField] private int ammoLeft;
     public TMP_Text ammoText;
     void Start()
     {
         fpsCamera = Camera.main;
+        lr = laserGun.GetComponent<LineRenderer>();
     }
     // Update is called once per frame
     void Update()
@@ -51,6 +54,7 @@ public class PlayerLaserShooting : MonoBehaviour
                     rb.AddForce(-hit.normal * hitForce);
                 }
             }
+            StartCoroutine(ShowLaserShot(hit.transform));
         }
         UpdateAmmoTextCanvas();
     }
@@ -64,5 +68,19 @@ public class PlayerLaserShooting : MonoBehaviour
     public void UpdateAmmoTextCanvas()
     {
         ammoText.text = ammoLeft.ToString();
+    }
+
+    IEnumerator ShowLaserShot(Transform target)
+    {
+
+        lr.SetPosition(0, muzzlePoint.transform.position);
+        lr.SetPosition(1, target.position);
+        lr.enabled = true;
+        Debug.Log(target.position);
+        // audioSource.Play();
+        muzzleFlash.Play();
+
+        yield return new WaitForSeconds(0.15f);
+        lr.enabled = false;
     }
 }
